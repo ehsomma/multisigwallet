6567a0bc3a7d55242b6d8a1fa88226838a18f7cf
ehsomma <ehsomma@gmail.com>
2022-03-30 22:23:14 -0300

    Migration file.

--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -1,16 +1,18 @@
 const MultiSigWallet = artifacts.require("MultiSigWallet");
+const TestContract = artifacts.require("TestContract");
 
-module.exports = async function(deployer, network, accounts) {
-  if (network === "main") {
-    return
-  }
+module.exports = async function (deployer, network, accounts) {
+    if (network === "main") {
+        return
+    }
 
-  console.log("-----------------------------")
-  console.log(accounts)
-  console.log("-----------------------------")
+    console.log("-----------------------------");
+    console.log(accounts);
+    console.log("-----------------------------");
 
-  const owners = await accounts.slice(0, 3);
-  const numConfirmationsRequired = 2;
+    const owners = await accounts.slice(0, 3);
+    const numConfirmationsRequired = 2;
 
-  await deployer.deploy(MultiSigWallet, owners, numConfirmationsRequired)
+    await deployer.deploy(MultiSigWallet, owners, numConfirmationsRequired);
+    await deployer.deploy(TestContract);
 }
